669cffa1fe355d82381d8fd313324771d57f13d3
Diego Lopez <lopezleondiego0@gmail.com>
2023-08-22 22:31:44 -0500

    Cambiamos el continente a Africano

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def run():
     percentages = list(map(lambda x: x["World Population Percentage"], data))"""
 
     df = pd.read_csv("data.csv")
-    df = df[df["Continent"] == "South America"]
+    df = df[df["Continent"] == "Africa"]
 
     # Seleccionamos la columna
     countries = df["Country"].values
