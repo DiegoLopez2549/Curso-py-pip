18ff8fceb2980a02da73a94dfa491c943b3ebb68
Diego Lopez <lopezleondiego0@gmail.com>
2023-09-06 22:31:44 -0500

    Se agregó la api con respuesta HTML

--- web_server/main.py
+++ web_server/main.py
@@ -1,4 +1,29 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def get_list():
+    return [1, 2, 3, 4]
+
+
+@app.get("/contact", response_class=HTMLResponse)
+def get_list():
+    return """
+    <html>
+        <head>
+            <title>Web FastAPI</title>
+        </head>
+        <body>
+            <h1>¡Hola a todos!</h1>
+            <p>Este es un texto de prueba para mi página</p>
+        </body>
+    </html>
+    """
 
 
 def run():
